0e55c6b5668ea30e132d5b3d1b5007aa43158a95
anastasiia-annata <itriA50562@itri.org.tw>
2019-02-27 17:46:47 +0800

    Save work. Fix y_true data type. Add y_true preprocessing functions and img resize. TODO: connect y_true preprocessing and img resize

--- luminoth/models/yolov3/yolov3.py
+++ luminoth/models/yolov3/yolov3.py
@@ -93,6 +93,7 @@ class YoloV3(snt.AbstractModule):
         self.feature_maps = [] # [[None, 13, 13, 255], [None, 26, 26, 255], [None, 52, 52, 255]]
         self.image_shape = [config.dataset.image_preprocessing.fixed_height,
                             config.dataset.image_preprocessing.fixed_width]
+        self._config=config
 
     def _yolo_block(self, inputs, filters):
         inputs = _conv2d_fixed_padding(inputs, filters * 1, 1)
@@ -173,12 +174,23 @@ class YoloV3(snt.AbstractModule):
         """
         # it will be needed later on
         #self.img_size = tf.shape(inputs)[1:3]
+        #print("inputs*******************************")
+        #print(inputs)
+        #print("y_true*******************************")
+        y_true = tf.dtypes.cast(y_true, tf.float32)
+
+        #print(y_true)
         self.img_size = tf.constant(list(self.image_shape))
+        #print("img_size*****************************")
+        #print(self.img_size)
         # [N H W C]
         self.image_shape.append(3)  # Add channels to shape
         inputs.set_shape(self.image_shape)
         # [HWC] to [N H W C]
         inputs = tf.expand_dims(inputs, 0, name='hardcode_batch_size_to_1')
+
+        #print("inputs after************************")
+        #print(inputs)
         # set batch norm params
         batch_norm_params = {
             'decay': self._BATCH_NORM_DECAY,
@@ -385,12 +397,12 @@ class YoloV3(snt.AbstractModule):
         # pred_box_??: [N, 13, 13, 3, 2]
 
         # shape: [N, 13, 13, 3, 1, 2]
-        print(true_box_xy.dtype)
-        print(tf.float32)
-        if true_box_xy.dtype != tf.float32:
-            true_box_xy = tf.dtypes.cast(true_box_xy, dtype=tf.float32)
-        if true_box_wh.dtype != tf.float32:
-            true_box_wh = tf.dtypes.cast(true_box_wh, dtype=tf.float32)
+        #print(true_box_xy.dtype)
+        #print(tf.float32)
+        #if true_box_xy.dtype != tf.float32:
+        #    true_box_xy = tf.dtypes.cast(true_box_xy, dtype=tf.float32)
+        #if true_box_wh.dtype != tf.float32:
+        #    true_box_wh = tf.dtypes.cast(true_box_wh, dtype=tf.float32)
 
         pred_box_xy = tf.expand_dims(pred_box_xy, -2)
         pred_box_wh = tf.expand_dims(pred_box_wh, -2)
@@ -399,10 +411,10 @@ class YoloV3(snt.AbstractModule):
         true_box_xy = tf.expand_dims(true_box_xy, 0)
         true_box_wh = tf.expand_dims(true_box_wh, 0)
 
-        print(pred_box_xy)
-        print(pred_box_wh)
-        print(true_box_xy)
-        print(true_box_wh)
+        #print(pred_box_xy)
+        #print(pred_box_wh)
+        #print(true_box_xy)
+        #print(true_box_wh)
         # [N, 13, 13, 3, 1, 2] & [1, V, 2] ==> [N, 13, 13, 3, V, 2]
         intersect_mins = tf.maximum(pred_box_xy - pred_box_wh / 2.,
                                     true_box_xy - true_box_wh / 2.)
@@ -420,3 +432,139 @@ class YoloV3(snt.AbstractModule):
         iou = intersect_area / (pred_box_area + true_box_area - intersect_area)
 
         return iou
+
+    def get_trainable_vars(self):
+        """Get trainable vars included in the module.
+        """
+        trainable_vars = snt.get_variables_in_module(self)
+        if False: #self._config.model.base_network.trainable:  TODO
+            pretrained_trainable_vars = (
+                self.feature_extractor.get_trainable_vars()
+            )
+            tf.logging.info('Training {} vars from pretrained module.'.format(
+                len(pretrained_trainable_vars)))
+            trainable_vars += pretrained_trainable_vars
+        else:
+            tf.logging.info('Not training variables from pretrained module')
+
+        return trainable_vars
+
+
+    def get_base_network_checkpoint_vars(self):
+        return None #TODO
+
+    def get_checkpoint_file(self):
+        return None #TODO
+    
+    def summary(self):
+        """
+        Generate merged summary of all the sub-summaries used inside the
+        network.
+        """
+        #summaries = [
+        #    tf.summary.merge_all()
+        #]
+
+        #return tf.summary.merge(summaries)
+        return None #TODO
+
+    def preprocess(self, image, gt_boxes):
+        # resize_image_correct_bbox
+        image, gt_boxes = self.resize_image_correct_bbox(image, gt_boxes, self.image_shape[0], self.image_shape[1])
+        if self.debug: return image, gt_boxes
+
+        y_true_13, y_true_26, y_true_52 = tf.py_func(self.preprocess_true_boxes, inp=[gt_boxes],
+                            Tout = [tf.float32, tf.float32, tf.float32])
+        # data augmentation
+        # pass
+        image = image / 255.
+
+        return image, y_true_13, y_true_26, y_true_52
+
+    def preprocess_true_boxes(self, gt_boxes):
+        """
+        Preprocess true boxes to training input format
+        Parameters:
+        -----------
+        :param true_boxes: numpy.ndarray of shape [T, 4]
+                            T: the number of boxes in each image.
+                            4: coordinate => x_min, y_min, x_max, y_max
+        :param true_labels: class id
+        :param input_shape: the shape of input image to the yolov3 network, [416, 416]
+        :param anchors: array, shape=[9,2], 9: the number of anchors, 2: width, height
+        :param num_classes: integer, for coco dataset, it is 80
+        Returns:
+        ----------
+        y_true: list(3 array), shape like yolo_outputs, [13, 13, 3, 85]
+                            13:cell szie, 3:number of anchors
+                            85: box_centers, box_sizes, confidence, probability
+        """
+        num_layers = len(self.anchors) // 3
+        anchor_mask = [[6,7,8], [3,4,5], [0,1,2]] if num_layers==3 else [[3,4,5], [1,2,3]]
+        grid_sizes = [[self.image_shape[0]//x, self.image_shape[1]//x] for x in (32, 16, 8)]
+
+        box_centers = (gt_boxes[:, 0:2] + gt_boxes[:, 2:4]) / 2 # the center of box
+        box_sizes =    gt_boxes[:, 2:4] - gt_boxes[:, 0:2] # the height and width of box
+
+        gt_boxes[:, 0:2] = box_centers
+        gt_boxes[:, 2:4] = box_sizes
+
+        y_true_13 = np.zeros(shape=[grid_sizes[0][0], grid_sizes[0][1], 3, 5+self.num_classes], dtype=np.float32)
+        y_true_26 = np.zeros(shape=[grid_sizes[1][0], grid_sizes[1][1], 3, 5+self.num_classes], dtype=np.float32)
+        y_true_52 = np.zeros(shape=[grid_sizes[2][0], grid_sizes[2][1], 3, 5+self.num_classes], dtype=np.float32)
+
+        y_true = [y_true_13, y_true_26, y_true_52]
+        anchors_max =  self._ANCHORS / 2.
+        anchors_min = -anchors_max
+        valid_mask = box_sizes[:, 0] > 0
+
+        # Discard zero rows.
+        wh = box_sizes[valid_mask]
+        # set the center of all boxes as the origin of their coordinates
+        # and correct their coordinates
+        wh = np.expand_dims(wh, -2)
+        boxes_max = wh / 2.
+        boxes_min = -boxes_max
+
+        intersect_mins = np.maximum(boxes_min, anchors_min)
+        intersect_maxs = np.minimum(boxes_max, anchors_max)
+        intersect_wh   = np.maximum(intersect_maxs - intersect_mins, 0.)
+        intersect_area = intersect_wh[..., 0] * intersect_wh[..., 1]
+        box_area       = wh[..., 0] * wh[..., 1]
+
+        anchor_area = self.anchors[:, 0] * self.anchors[:, 1]
+        iou = intersect_area / (box_area + anchor_area - intersect_area)
+        # Find best anchor for each true box
+        best_anchor = np.argmax(iou, axis=-1)
+
+        for t, n in enumerate(best_anchor):
+            for l in range(num_layers):
+                if n not in anchor_mask[l]: continue
+
+                i = np.floor(gt_boxes[t,0]/self.image_w*grid_sizes[l][1]).astype('int32')
+                j = np.floor(gt_boxes[t,1]/self.image_h*grid_sizes[l][0]).astype('int32')
+
+                k = anchor_mask[l].index(n)
+                c = gt_boxes[t, 4].astype('int32')
+
+                y_true[l][j, i, k, 0:4] = gt_boxes[t, 0:4]
+                y_true[l][j, i, k,   4] = 1.
+                y_true[l][j, i, k, 5+c] = 1.
+
+        return y_true_13, y_true_26, y_true_52
+
+    def resize_image_correct_bbox(image, boxes, image_h, image_w):
+
+        origin_image_size = tf.to_float(tf.shape(image)[0:2])
+        image = tf.image.resize_images(image, size=[image_h, image_w])
+
+        # correct bbox
+        xx1 = boxes[:, 0] * image_w / origin_image_size[1]
+        yy1 = boxes[:, 1] * image_h / origin_image_size[0]
+        xx2 = boxes[:, 2] * image_w / origin_image_size[1]
+        yy2 = boxes[:, 3] * image_h / origin_image_size[0]
+        idx = boxes[:, 4]
+
+        boxes = tf.stack([xx1, yy1, xx2, yy2, idx], axis=1)
+        return image, boxes
+
